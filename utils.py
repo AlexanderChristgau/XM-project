89399975075a3a532dfaa23c967d63773e77d647
Alexander <alex58733@live.dk>
2021-07-31 15:21:31 +0200

    prototype relations

--- utils.py
+++ utils.py
@@ -26,5 +26,35 @@ def partialterm(l,i,b):
     return b[i-3]+1
 
 
+def accel_asc(n):
+    a = [0 for i in range(n + 1)]
+    k = 1
+    y = n - 1
+    while k != 0:
+        x = a[k - 1] + 1
+        k -= 1
+        while 2 * x <= y:
+            a[k] = x
+            y -= x
+            k += 1
+        l = k + 1
+        while x <= y:
+            a[k] = x
+            a[l] = y
+            yield a[:k + 2]
+            x += 1
+            y -= 1
+        a[k] = x + y
+        y = x + y - 1
+        yield a[:k + 1]
 
+
+def to_ctype(l):
+    answer = np.zeros(max(l),dtype=int)
+    for i in l:
+        answer[i-1] += 1
+    return tuple(answer)
+
+def generate_cycletypes(d):
+    return (to_ctype(partition) for partition in accel_asc(d))
 
